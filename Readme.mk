4cc91efed78bf8206f2dc9e5c143f242025d8c2a
Jean-Baptiste Baillet <jbbaillet.dev@gmail.com>
2020-10-07 11:22:00 +0200

    creat requirements and readme

---
+++ Readme.mk
@@ -0,0 +1,28 @@
+# Initiez-vous à Python pour l'analyse de données 
+
+Description:
+
+Objectif du programme:
+
+
+Fonctionnalités du programme:
+
+
+Pour installer le programme:
+
+- nécéssite python
+
+Créer un environnement virtuel:
+windows: py -3.8 -m venv venv
+mac/linux: python3 -m venv venv
+
+Activer l'environement virtuel:
+windows: source venv\Scripts\activate
+mac/linux: source/bin/activate
+
+Installer la dépendance jupyter:
+pip3 install -r requirements.txt
+
+Lancer le programme:
+windows: python main.py
+mac/linux: python3 main.py
